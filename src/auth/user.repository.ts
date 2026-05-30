72cd1132be63221cd889b1992261311b15cadfa7
jungmin <kjmhs501@gmail.com>
2026-05-30 18:15:58 +0900

    feat: 비밀번호 암호화

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,6 +2,7 @@ import { DataSource, QueryFailedError, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -12,9 +13,11 @@ export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = new User();
-    user.username = username;
-    user.password = password;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
+
     try {
       await this.save(user);
     } catch (error) {
