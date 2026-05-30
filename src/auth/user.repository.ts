a3af8039e66c5e5c2a8a07729ef056e762fc5c9f
jungmin <kjmhs501@gmail.com>
2026-05-30 18:05:02 +0900

    feat: 회원가입 시 중복 이름 에러처리

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,6 @@
-import { DataSource, Repository } from 'typeorm';
+import { DataSource, QueryFailedError, Repository } from 'typeorm';
 import { User } from './user.entity';
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
 @Injectable()
@@ -12,7 +12,17 @@ export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = this.create({ username, password });
-    await this.save(user);
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error instanceof QueryFailedError && (error as any).code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
