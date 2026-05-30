c6c7b0623d85da25c70c86265e370be11be2054d
jungmin <kjmhs501@gmail.com>
2026-05-30 17:44:04 +0900

    feat: auth 모듈 생성 + reposiroty 연결

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
-import { Board } from './board.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Board])],
+  imports: [],
   controllers: [BoardsController],
   providers: [BoardsService, BoardRepository],
 })
