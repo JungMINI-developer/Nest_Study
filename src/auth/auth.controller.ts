89c7b8935684ad2d153c49cda48f3950597effed
jungmin <kjmhs501@gmail.com>
2026-05-31 15:45:03 +0900

    feat: 커스텀 데코레이터로 user객체만 request에서 가져오기

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUSer } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -21,7 +23,7 @@ export class AuthController {
 
   @Post('/authTest')
   @UseGuards(AuthGuard())
-  authtest(@Req() req) {
-    console.log('req', req);
+  authtest(@GetUSer() user: User) {
+    console.log('user', user);
   }
 }
