85ee2f9fa6384d2c613a67b5284646b142bc1d62
jungmin <kjmhs501@gmail.com>
2026-05-31 15:48:10 +0900

    feat: boards에서 로그인 된 사람들만 api를 이용할 수 있도록 useGards 설정

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { BoardRepository } from './board.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [BoardsController],
   providers: [BoardsService, BoardRepository],
 })
